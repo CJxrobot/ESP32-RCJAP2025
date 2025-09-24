7e2fa82fa8eb2f45f964ac62bf32a7d1d2010fa3
CJxrobot <165867124+CJxrobot@users.noreply.github.com>
2025-09-24 23:32:05 +0800

    Update 1-DualCore-LED-Serial.ino

--- Basic/1-DualCore-LED-Serial/1-DualCore-LED-Serial.ino
+++ Basic/1-DualCore-LED-Serial/1-DualCore-LED-Serial.ino
@@ -1,1 +1,36 @@
+// Example 1: Dual Core - LED + Serial Print
+
+#define LED_BUILTIN 2
+
+void TaskBlink(void *pvParameters);
+void TaskPrint(void *pvParameters);
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+
+  // Task on Core 0: LED blinking
+  xTaskCreatePinnedToCore(TaskBlink, "Blink", 2000, NULL, 1, NULL, 0);
+
+  // Task on Core 1: Serial printing
+  xTaskCreatePinnedToCore(TaskPrint, "Print", 2000, NULL, 1, NULL, 1);
+}
+
+void TaskBlink(void *pvParameters) {
+  while (1) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskPrint(void *pvParameters) {
+  while (1) {
+    Serial.println("Hello from Core 1");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void loop() {
+  // Empty
+}
 
