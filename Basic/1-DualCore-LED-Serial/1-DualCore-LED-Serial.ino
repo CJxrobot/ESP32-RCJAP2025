3821eb77ed376794b274d850ffe20dcce4787e8a
CJxrobot <165867124+CJxrobot@users.noreply.github.com>
2025-09-24 23:47:23 +0800

    Update 1-DualCore-LED-Serial.ino

--- Basic/1-DualCore-LED-Serial/1-DualCore-LED-Serial.ino
+++ Basic/1-DualCore-LED-Serial/1-DualCore-LED-Serial.ino
@@ -1,36 +1,56 @@
-// Example 1: Dual Core - LED + Serial Print
+// Example 1 (Practice): Dual Core - LED + Serial Print
+// Students: Fill in the TODO parts and test on your ESP32
 
-#define LED_BUILTIN 2
+#define LED_BUILTIN 2   // Built-in LED pin
 
-void TaskBlink(void *pvParameters);
-void TaskPrint(void *pvParameters);
+// Step 1: Declare the task functions
+void /* TODO: TaskBlink */(void *pvParameters);
+void /* TODO: TaskPrint */(void *pvParameters);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
+  // Step 2: Set LED pin as output
+  pinMode(LED_BUILTIN, /* TODO: OUTPUT */);
 
-  // Task on Core 0: LED blinking
-  xTaskCreatePinnedToCore(TaskBlink, "Blink", 2000, NULL, 1, NULL, 0);
+  // Step 3: Begin Serial communication at 115200 baud
+  Serial.begin(/* TODO: baud rate */);
 
-  // Task on Core 1: Serial printing
-  xTaskCreatePinnedToCore(TaskPrint, "Print", 2000, NULL, 1, NULL, 1);
+  // Step 4: Create a task for LED blinking on Core 0
+  xTaskCreatePinnedToCore(
+      /* TODO: TaskBlink */,   // Function
+      "Blink",                 // Task name
+      2000,                    // Stack size
+      NULL,                    // Input parameter
+      1,                       // Priority
+      NULL,                    // Task handle
+      /* TODO: core number */);
+
+  // Step 5: Create a task for printing on Core 1
+  xTaskCreatePinnedToCore(
+      /* TODO: TaskPrint */,
+      "Print",
+      2000,
+      NULL,
+      1,
+      NULL,
+      /* TODO: core number */);
 }
 
-void TaskBlink(void *pvParameters) {
+// Task 1: Blink LED
+void /* TODO: TaskBlink */(void *pvParameters) {
   while (1) {
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // toggle LED
+    vTaskDelay(/* TODO: delay time (ms) */ / portTICK_PERIOD_MS);
   }
 }
 
-void TaskPrint(void *pvParameters) {
+// Task 2: Print message
+void /* TODO: TaskPrint */(void *pvParameters) {
   while (1) {
-    Serial.println("Hello from Core 1");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Serial.println(/* TODO: message string */);
+    vTaskDelay(/* TODO: delay time (ms) */ / portTICK_PERIOD_MS);
   }
 }
 
 void loop() {
-  // Empty
+  // Empty, tasks are running on cores
 }
-
