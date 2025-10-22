84136d44f125d45fdfc0b68a0ecdec66f716ac8d
CJxrobot <165867124+CJxrobot@users.noreply.github.com>
2025-10-22 14:08:26 +0800

    Update 4_dir_localize.ino

--- Localization/4_dir_localize/4_dir_localize.ino
+++ Localization/4_dir_localize/4_dir_localize.ino
@@ -17,7 +17,45 @@
 
 
 
+void parseOutput(const char* output) {
+  
+    // 检查是否是特定的 "0\r\n" 字符串
+    if (strcmp(output, "0\r") == 0) {
+        range = amplitude = ringdown_flag = 0;
+        return 0;
+    } 
+    else if (strstr(output, "Range:") != NULL && strstr(output, "Amplitude:") != NULL) {
+        // 找到输出字符串中 "Range:" 和 "Amplitude:" 的位置
+        const char* rangePtr = strstr(output, "Range:");
+        const char* amplitudePtr = strstr(output, "Amplitude:");
+        const char* ringdownPtr = strstr(output, "Ringdown_flag:");
 
+        if (ringdownPtr == NULL) {
+            ringdown_flag = -1; // 如果没有 "Ringdown_flag:"，则默认值为 -1
+        } else {
+            // 找到ringdown_flag的值的起始位置
+            const char* ringdownStartPtr = ringdownPtr + strlen("Ringdown_flag:");
+            // 从字符串中解析出ringdown_flag的值
+            char* ringdownEndPtr;
+            ringdown_flag = (int)strtol(ringdownStartPtr, &ringdownEndPtr, 10);
+        }
+
+        // 找到range和amplitude的值的起始位置
+        const char* rangeStartPtr = rangePtr + strlen("Range:");
+        const char* amplitudeStartPtr = amplitudePtr + strlen("Amplitude:");
+
+        // 从字符串中解析出range的值
+        char* rangeEndPtr;
+        range = (int)strtol(rangeStartPtr, &rangeEndPtr,10);
+
+        // 从字符串中解析出amplitude的值
+        char* amplitudeEndPtr;
+        amplitude = (int)strtol(amplitudeStartPtr, &amplitudeEndPtr, 10);
+    } else {
+        // 如果字符串格式不正确，将range、amplitude和ringdown_flag都设置为-1
+        range = amplitude = ringdown_flag = -1;
+    }
+}
 
 void setup(){
   Serial.begin(115200);
